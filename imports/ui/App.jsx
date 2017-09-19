9e87f69b5cfb4bdcd0e2fecb7b4e82e42223669d
Koen Castermans <castermanskoen@msn.com>
2017-09-19 21:19:18 +0200

    Add beginning React files

---
+++ imports/ui/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                <header>
+                    
+                </header>
+            </div>
+        )
+    }
+}
