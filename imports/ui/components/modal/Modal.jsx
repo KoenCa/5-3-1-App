e2890f94e893d65d52f7b95f6c063c1e077b69e5
Koen Castermans <castermanskoen@msn.com>
2017-10-14 19:19:53 +0200

    Make reusable modal component importable via one file

--- imports/ui/components/modal/Modal.jsx
+++ imports/ui/components/modal/Modal.jsx
@@ -1,27 +1,6 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import Modal from './ModalContainer';
+import ModalHeader from './ModalHeader';
+import ModalBody from './ModalBody';
+import ModalFooter from './ModalFooter';
 
-export default class Modal extends Component {
-  componentDidMount() {
-    const modalId = `#${this.props.modalName}`
-    $(modalId).on('hidden.bs.modal', this.props.onModalClose);
-    $(modalId).modal()
-  }
-
-  render() {
-    return (
-      <div className="modal fade" id={this.props.modalName} tabIndex="-1" role="dialog">
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            {this.props.children}
-          </div>
-        </div>
-      </div>
-    )
-  }
-}
-
-Modal.propTypes = {
-  modalName: PropTypes.string.isRequired,
-  onModalClose: PropTypes.func.isRequired
-}
+export { Modal, ModalHeader, ModalBody, ModalFooter }
---
+++ imports/ui/components/modal/ModalContainer.jsx
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+export default class Modal extends Component {
+  componentDidMount() {
+    const modalId = `#${this.props.modalName}`
+    $(modalId).on('hidden.bs.modal', this.props.onModalClose);
+    $(modalId).modal()
+  }
+
+  render() {
+    return (
+      <div className="modal fade" id={this.props.modalName} tabIndex="-1" role="dialog">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            {this.props.children}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+Modal.propTypes = {
+  modalName: PropTypes.string.isRequired,
+  onModalClose: PropTypes.func.isRequired
+}
