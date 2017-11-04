bb018b901a00271a61b64e3b851bff47a942c8da
Koen Castermans <castermanskoen@msn.com>
2017-11-04 11:14:13 +0100

    Change email method: fix bug

--- imports/api/accounts.js
+++ imports/api/accounts.js
@@ -3,23 +3,22 @@ import {Accounts} from 'meteor/accounts-base';
 import {check} from 'meteor/check';
 
 Meteor.methods({
-    'accounts.changeEmail'(email) {
-        check(email, String);
-        const user = Meteor.user();
-        const oldEmail = user.emails[0].address;
+  'accounts.changeEmail' (email) {
+    check(email, String);
+    const user = Meteor.user();
+    const oldEmail = user.emails[0].address;
 
-        if (!user) {
-            throw new Meteor.Error('Not authorized');
-        }
+    if (!user) {
+      throw new Meteor.Error('Not authorized');
+    }
 
-        Accounts.removeEmail(user._id, oldEmail);
+    Accounts.removeEmail(user._id, oldEmail);
 
-        try {
-            Accounts.addEmail(user._id, email);
-        } catch (e) {
-            throw e;
-        } finally {
-            Accounts.addEmail(user._id, oldEmail);
-        }
-    },
+    try {
+      Accounts.addEmail(user._id, email);
+    } catch (e) {
+      Accounts.addEmail(user._id, oldEmail);
+      throw e;
+    }
+  }
 })
