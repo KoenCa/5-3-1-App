f2c235abbad796a2d3d59bbd028850e3ee100f15
Koen Castermans <castermanskoen@msn.com>
2017-11-12 12:03:21 +0100

    ModalFooter: remove white space

--- imports/ui/components/modal/ModalFooter.jsx
+++ imports/ui/components/modal/ModalFooter.jsx
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 export default class ModalBody extends Component {
-
   render() {
     return (
       <div className="modal-footer">
