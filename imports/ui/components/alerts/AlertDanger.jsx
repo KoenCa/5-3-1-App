18f156705dc8e9b1774f932957cd43debcc74dd8
Koen Castermans <castermanskoen@msn.com>
2017-11-12 10:19:59 +0100

    Alert danger: remove method

--- imports/ui/components/alerts/AlertDanger.jsx
+++ imports/ui/components/alerts/AlertDanger.jsx
@@ -2,9 +2,6 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 export default class AlertDanger extends Component {
-
-  shouldComponentUpdate = () => true
-
   render() {
     return (
       <div className="alert alert-danger" role="alert">
