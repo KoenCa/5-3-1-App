548c94bcee7a243de801013ce1d163c78ba67380
Koen Castermans <castermanskoen@msn.com>
2017-12-09 21:42:21 +0100

    Modal.jsx -> Modal.js (rename)

--- imports/ui/components/modal/Modal.jsx
+++
@@ -1,6 +0,0 @@
-import Modal from './ModalContainer';
-import ModalHeader from './ModalHeader';
-import ModalBody from './ModalBody';
-import ModalFooter from './ModalFooter';
-
-export { Modal, ModalHeader, ModalBody, ModalFooter }
